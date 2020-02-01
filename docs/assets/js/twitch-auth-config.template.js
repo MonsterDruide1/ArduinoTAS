4bfe3967252655991f93f9f821a8c1074d626646
MonsterDruide1 <5958456@gmail.com>
2020-02-01 16:02:37 +0100

    Removed unused things

--- docs/assets/js/twitch-auth-config.template.js
+++
@@ -1,5 +0,0 @@
-// CONFIG: twitch oauth settings
-window.twitch_auth_config = {
-    clientId: PLEASE_CONFIG_ME, // from [Twitch Client ID](https://glass.twitch.tv/console/apps)
-    redirect: window.location.origin + '/gamepad.html', // from [Twitch OAuth Redirect URL](https://glass.twitch.tv/console/apps)
-};
